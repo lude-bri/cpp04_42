a451641c69b8dc81cfa5d1cb0ceadb8d447246f8
Luigi de Brito Piantavinha <szochoralewizc@hotmail.com>
2025-06-20 23:35:35 +0100

    cpp04 - ex00 - wrong animal

--- ex00/WrongAnimal.cpp
+++ ex00/WrongAnimal.cpp
@@ -6,17 +6,31 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 23:24:33 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/06/20 23:30:49 by luigi            ###   ########.fr       */
+/*   Updated: 2025/06/20 23:35:27 by luigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "WrongAnimal.hpp"
 
+//Default constructor
 WrongAnimal::WrongAnimal() {
 	type = "Wrong Animal";
 	std::cout << "Wrong Animal was created" << std::endl;
 }
 
+//Default copy constructor
+WrongAnimal::WrongAnimal(const WrongAnimal &copy) {
+	*this = copy;
+	std::cout << "A WrongAnimal copy was created\n";
+}
+
+//Default copy operator assignment
+WrongAnimal &WrongAnimal::operator=(const WrongAnimal &copy) {
+	std::cout << "A WrongAnimal operator assignment was created\n";
+	return (*this);
+}
+
+//Default destructor
 WrongAnimal::~WrongAnimal() {
 	std::cout << "Wrong Animal was destroyed" << std::endl;
 }
--- ex00/WrongAnimal.hpp
+++ ex00/WrongAnimal.hpp
@@ -6,14 +6,13 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 23:25:18 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/06/20 23:32:31 by luigi            ###   ########.fr       */
+/*   Updated: 2025/06/20 23:33:02 by luigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "Animal.hpp"
-#include <iostream>
 
 class WrongAnimal : public Animal {
 
