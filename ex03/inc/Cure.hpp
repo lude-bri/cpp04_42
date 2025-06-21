1d9211cd99d33934fcc4eb924430c1e0bef891c0
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-06-21 17:40:47 +0100

    cpp02 - ex03 - cure setup

--- ex03/inc/Cure.hpp
+++ ex03/inc/Cure.hpp
@@ -6,8 +6,22 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 17:25:01 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/06/21 17:26:08 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/06/21 17:40:41 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include "AMateria.hpp"
+
+class Cure {
+public:
+	Cure();
+	Cure(const Cure &); 
+	Cure &operator=(const Cure &);
+	~Cure();
+
+	AMateria	*clone() const;
+	void		use(ICharacter &target);
+};
 
